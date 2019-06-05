ec5c1c5d3a6a044ae7d58288531e7b6980c5aa4b
aviles.esp@gmail.com <aviles.esp@gmail.com>
2019-06-05 14:49:40 -0500

    Prueba2

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,23 @@
 window.cipher = {
   // ... 
 };
+
+
+//65 es A y 90 es Z cuando 97 es a y 122 es z
+// esto es un sistema americano de 1963 llamado ASCII
+
+
+//increible
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -4,4 +4,3 @@ var dado = document.getElementById('dado');
 function generarNumero() {
   var numero = Math.floor(Math.random() * 6 + 1);
   dado.textContent = numero;
-}
